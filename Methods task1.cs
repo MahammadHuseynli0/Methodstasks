3cf4cceafb0f2e802296deb3bb321349da00c228
MahammadHuseynli0 <tu7uv5kks.@code.edu.az>
2024-03-18 12:07:30 +0400

    methods

---
+++ Methods task1.cs
@@ -0,0 +1,27 @@
+﻿string[] cars1 = { "toyota", "bmw", "mercedes", "gelly" };
+string[] cars2 = { "ford", "mercedes", "porche" };
+
+SimilarString(cars1, cars2);
+
+
+
+
+ static void SimilarString(string[] a, string[] b)
+{
+    string SameWords = " ";
+    for (int i = 0; i < a.Length; i++)
+    {
+
+
+        for (int k = 0; k < b.Length; k++)
+        {
+            if (a[i] == b[k])
+            {
+
+                SameWords += a[i];
+            }
+        }
+    }
+    Console.WriteLine(SameWords);
+
+}
---
+++ Methodstask2.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+
+int[] arry1 = { 5, 9, 11, 12, 13 };
+int[] arry2 = { 6, 7, 14, 15, 17 };
+
+Answer(arry1, arry2);
+
+
+static void Answer(int[] a, int[] b)
+{
+    string product = " ";
+
+
+    for (int i = 0; i < a.Length; i++)
+    { 
+        product += a[i] * b[i]; 
+    }
+    Console.WriteLine(product);
+}
